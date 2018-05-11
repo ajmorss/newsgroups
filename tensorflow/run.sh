4cfff9a1283d487060fcdb980e2e947cca97652f
Andrew Morss <andrew.morss@accenture.com>
2018-05-11 14:22:44 -0700

    fixed run.sh

--- tensorflow/run.sh
+++ tensorflow/run.sh
@@ -1,21 +1,27 @@
-some_command &
-P1=$!
-other_command &
-P2=$!
-wait $P1 $P2
+#some_command &
+#P1=$!
+#other_command &
+#P2=$!
+#wait $P1 $P2
 
 python trainer.py \
+     --distributed=True \
      --ps_hosts=localhost:2222 \
      --worker_hosts=localhost:2223,localhost:2221 \
-     --job_name=ps --task_index=0
+     --job_name=ps --task_index=0 \
+     --config_yaml=config/sample.yaml 
 # On worker0.example.com:
 python trainer.py \
-     --ps_hosts=ps0.example.com:2222 \
+     --distributed=True \
+     --ps_hosts=localhost:2222 \
      --worker_hosts=localhost:2223,localhost:2221 \
-     --job_name=worker --task_index=0
+     --job_name=worker --task_index=0 \
+     --config_yaml=config/sample.yaml
 # On worker1.example.com:
 python trainer.py \
-     --ps_hosts=ps0.example.com:2222 \
+     --distributed=True \
+     --ps_hosts=localhost:2222 \
      --worker_hosts=localhost:2223,localhost:2221 \
-     --job_name=worker --task_index=1
+     --job_name=worker --task_index=1 \
+     --config_yaml=config/sample.yaml
 
